1985cb392127303b53c8e2345dcb666164f78593
Rutuj Runwal <rutujrunwal001@gmail.com>
2023-09-20 01:15:02 +0530

    Updated json handling to fix quarantine data override

--- backend/parseJson.py
+++ backend/parseJson.py
@@ -5,35 +5,37 @@ class ParseJson:
         if not os.path.exists(configFilePath):
             os.mkdir(configFilePath)
         self.PATH = os.path.join(configFilePath, configFileName+'.json')
+        self.defaults = defaults
         self.data = self.parseDataFile(defaults)
     
     def getVal(self,key):
         return self.data[key]
     
     def setVal(self,key,val):
+        self.data = self.parseDataFile(self.defaults)
         self.data[key] = val
         with open(self.PATH, 'w') as file_obj:
             json.dump(self.data, file_obj)
-            file_obj.close()
+            #file_obj.close()
 
     def removeVal(self,key):
         with open(self.PATH,'r') as fp:
                 parsed_data = json.load(fp)
-                fp.close()
+                #fp.close()
         del parsed_data[key]
         with open(self.PATH, 'w') as fp:
             json.dump(parsed_data, fp)
-            fp.close()
+            #fp.close()
 
     def parseDataFile(self,defaults):
         try:
             with open(self.PATH,'r') as fp:
                 parsed_data = json.load(fp)
-                fp.close()
+                #fp.close()
             return parsed_data
         except Exception as e:
             # print(e)
             with open(self.PATH, 'w') as fp:
                 json.dump(defaults, fp)
-                fp.close()
+                #fp.close()
             return defaults
