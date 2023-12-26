0ae6ca6612edb32a836d6028f53e32c5ed5a484f
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-12-26 16:30:34 +0300

    Update scanner.py

--- backend/scanner.py
+++ backend/scanner.py
@@ -1,96 +1,90 @@
 import hashlib
 import os
-import yara
 from quarantineThreats import Quarantine
 
 class Scanner:
-    fileTypes = [".vbs", ".ps", ".ps1", ".rar", ".tmp", ".bas", ".bat", ".chm", ".cmd", ".com", ".cpl", ".crt", ".dll", ".exe", ".hta", ".js", ".lnk", ".msc", ".ocx", ".pcd", ".pif", ".pot", ".pdf", ".reg", ".scr", ".sct", ".sys", ".url", ".vb", ".vbe", ".wsc", ".wsf", ".wsh", ".ct", ".t", ".input", ".war",".jsp", ".jspx", ".php", ".asp", ".aspx", ".doc", ".docx", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx", ".tmp", ".log", ".dump", ".pwd", ".w", ".txt", ".conf", ".cfg", ".conf", ".config", ".psd1", ".psm1", ".ps1xml", ".clixml", ".psc1", ".pssc", ".pl", ".www", ".rdp", ".jar", ".docm", ".sys", ".zip", ".tar",".msi"]
-
-    def __init__(self,signatures,rootPath):
-        import sys
-        self.__signatures = signatures
-        if getattr(sys,'frozen',False):
-            # Current Path
-            self.__rootPath = "."
-        else:
-            self.__rootPath = rootPath
+    def __init__(self, sha256_signatures, md5_signatures, rootPath, yara_rules):
+        self.__sha256_signatures = sha256_signatures
+        self.__md5_signatures = md5_signatures
+        self.__rootPath = rootPath
+        self.yara_rules = yara_rules  # Replace with your actual YARA rules
         self.quarantineData = {
-            'configFileName':'quar_info',
-            'configFilePath':self.__rootPath+'/config/',
-            'defaults':{}
+            'configFileName': 'quar_info',
+            'configFilePath': os.path.join(self.__rootPath, 'config'),
+            'defaults': {}
         }
-        try:
-            while not os.path.exists(self.__rootPath+"/compiledRules"):
-                print("Waiting for resources to compile...")
-            # Initialize yara rules
-            self.peid_rules = yara.compile(filepath=self.__rootPath+"/compiledRules")
-        except Exception as e:
-            print(e)
         print("-----Scanner Initialized-----")
         self.quar = Quarantine(self.quarantineData)
+        
+        # Read excluded rule names from the file
+        excluded_rules_path = os.path.join(self.__rootPath, 'excluded', 'excluded_rules.txt')
+        with open(excluded_rules_path, "r") as file:
+            self.excluded_rules = file.read()
 
-    def getFileHash(self,path):
+    def getFileHash(self, path):
         hash = ""
         try:
-            with open(path,'rb') as f:
+            with open(path, 'rb') as f:
                 bytes = f.read()
                 hash = hashlib.sha256(bytes).hexdigest()
-            if hash!="":
+            if hash != "":
                 return hash
         except (PermissionError, OSError):
             print("Permission Error")
             return "XYLENT_PERMISSION_ERROR"
-    
-    def verifyExecutableSignature(self,path):
+
+    def getMD5Hash(self, path):
+        hash = ""
+        try:
+            with open(path, 'rb') as f:
+                bytes = f.read()
+                hash = hashlib.md5(bytes).hexdigest()
+            if hash != "":
+                return hash
+        except (PermissionError, OSError):
+            print("Permission Error")
+            return "XYLENT_PERMISSION_ERROR"
+
+    def verifyExecutableSignature(self, path):
         import subprocess
-        import time,datetime
-        # TODO: Add a check for appropriate file extensions [.exe, .msi etc]
-        cmd =  " " + f'"{path}"'
+        import time
+        import datetime
+        cmd = " " + f'"{path}"'
         command = "(Get-AuthenticodeSignature" + cmd + ").Status"
         process = subprocess.run(['Powershell', '-Command', command], stdout=subprocess.PIPE, encoding='utf-8')
         now = time.time()
-        ageInSec = now-os.stat(path).st_mtime
+        ageInSec = now - os.stat(path).st_mtime
         age = str(datetime.timedelta(seconds=ageInSec))
-        # Valid - Trust
-        # NotSigned - Low Suspicion - Score: 30
-        # UnknownError - invalid # High Suspicion - Score: 50
-        # HashMismatch - treat as invalid # High Suspicion - Score: 50
-        # NotTrusted - Moderate Suspicion - Score: 40 
-        print(process.stdout.strip())
         if process.stdout.strip() == "HashMismatch" or process.stdout.strip() == "UnknownError":
-            return {'score':80,'age':age}
-        elif process.stdout.strip()=="NotTrusted":
-            return {'score':70,'age':age}
-        elif process.stdout.strip()=="NotSigned":
-            return {'score':30,'age':age}
+            return {'score': 80, 'age': age}
+        elif process.stdout.strip() == "NotTrusted":
+            return {'score': 70, 'age': age}
+        elif process.stdout.strip() == "NotSigned":
+            return {'score': 30, 'age': age}
         else:
-            return {'score':0,'age':age}
-    
+            return {'score': 0, 'age': age}
+
     def handleArchives(self, path):
         print("Handling Archive!!")
         import shutil
         try:
             archiveExtractPath = "./scanExtracts"
-            print("----------------------------------------------------")
-            print(archiveExtractPath.split("/")[1])
-            print(path)
-            print("----------------------------------------------------")
             if archiveExtractPath.split("/")[1] in path:
-                print("Skipped to avoid recursion.Depth=1 for scanning archives!")
+                print("Skipped to avoid recursion. Depth=1 for scanning archives!")
                 return "DONE!"
             else:
                 if not os.path.exists(archiveExtractPath):
                     os.mkdir(archiveExtractPath)
                 shutil.unpack_archive(path, archiveExtractPath)
                 verdicts = self.scanFolders(archiveExtractPath)
-                # dictionary in scanFolders leads to O(1) time
                 if "[S]" or "[Y]" in verdicts:
                     if os.path.exists(archiveExtractPath):
                         print("Malware detected in archive")
                         from notifypy import Notify
                         notification = Notify()
+                        # Set notification properties
                         notification.title = "Archive Repaired"
-                        notification.message = "Archive with malicious content repaired.Malware removed,Safe content Preserved!"
+                        notification.message = "Archive with malicious content repaired. Malware removed, Safe content Preserved!"
                         notification.send()
                         self.quar.quarantineFilesInArchive(originalZipPath=path, preserveArchiveContent=True)
 
@@ -98,85 +92,92 @@ class Scanner:
             print(e)
         return "DONE!"
 
-    def scanFile(self,path):
-        detectionSpace = "SAFE" 
+    def scanFile(self, path):
+        detectionSpace = "SAFE"
         suspScore = 0
         isArchive = False
-        fileExtension = ".unknown"
         try:
-            # TODO: use os.path.splitext or other effective alternatives
             fileExtension = os.path.splitext(path)[1]
+            hashToChk = self.getFileHash(path)
 
-            if not fileExtension == ".unknown" and fileExtension in self.fileTypes:
-                hashToChk = self.getFileHash(path)
-
-                if hashToChk == "XYLENT_PERMISSION_ERROR":
+            if hashToChk == "XYLENT_PERMISSION_ERROR":
                     return "SKIPPED"
 
-                if fileExtension == ".zip" or fileExtension == ".tar":
+            if fileExtension == ".zip" or fileExtension == ".tar":
                     isArchive = True
-                
-                if not isArchive and (fileExtension == ".exe" or fileExtension == ".msi"):
+
+            if not isArchive and (fileExtension == ".exe" or fileExtension == ".msi"):
                     print(path)
                     print("Analyzing file signature....")
                     exeSigData = self.verifyExecutableSignature(path)
                     print(exeSigData)
                     suspScore += exeSigData['score']
-                    if suspScore>=70:
+                    if suspScore >= 70:
                         detectionSpace = "Invalid Signature"
 
-                # Time Taken: O(n) 
-                # TODO: optimize using Binary search for O(log(n))
+            if hashToChk != "" and suspScore < 70:
+                    # SIGNATURE BASED DETECTION - SHA256
+                    sha256_match_found = False
+                    for sha256_hash in self.__sha256_signatures:
+                        if sha256_hash == str(hashToChk):
+                            print(self.__sha256_signatures[sha256_hash])
+                            detectionSpace = "[S]" + self.__sha256_signatures[sha256_hash]
+                            sha256_match_found = True
+                            break
 
-                # SIGNATURE BASED DETECTION
-                if hashToChk!="" and suspScore<70:
-                    for hash in self.__signatures:
-                        if hash==str(hashToChk):
-                            print(self.__signatures[hash])
-                            suspScore+=100
-                            detectionSpace = "[S]" + self.__signatures[hash]
+                    # SIGNATURE BASED DETECTION - MD5
+                    md5_match_found = False
+                    md5_hash = self.getMD5Hash(path)
+                    for md5_hash_sig in self.__md5_signatures:
+                        if md5_hash == md5_hash_sig:
+                            print(self.__md5_signatures[md5_hash_sig])
+                            detectionSpace = "[S]" + self.__md5_signatures[md5_hash_sig]
+                            md5_match_found = True
+                            break
 
+                    # Combine hash match checks
+                    if sha256_match_found or md5_match_found:
+                        # Set suspScore to 100 or any other value as needed
+                        suspScore = 100
                     # YARA RULES DETECTION
-                    if not isArchive and suspScore==0:
+                    if not isArchive:
                         try:
-                            matches = self.peid_rules.match(path)
-                            if matches:
+                            with open(path, 'rb') as f:
+                                file_content = f.read()
+                            yara_match_found = False
+                            for rule_name, compiled_rule in self.yara_rules.items():
+                                matches = compiled_rule.match(data=file_content)
                                 for match in matches:
-                                    if 'score' in match.meta:
-                                        suspScore += int(match.meta['score'])
-                                    else:
-                                        # ⚠ Better scoring mechanism needed, if no score is provided ⚠
-                                        suspScore+=20
-                                    detectionSpace = "[Y]"+ str(match)
-                            else:
-                                return "SAFE"
+                                    if match.rule not in self.excluded_rules:
+                                        # If any YARA rule matches, consider it as malware
+                                        print(f"YARA Rule Match: {rule_name} - {match}")
+                                        detectionSpace = "[Y]" + rule_name
+                                        yara_match_found = True
+                                        # Set suspScore to 100 or any other value as needed
+                                        suspScore = 100
+                                        break  # Exit loop if a match is found
+                            if yara_match_found:
+                                # Set suspScore to 100 or any other value as needed
+                                suspScore = 100
                         except Exception as e:
-                            # print("SKIPPED!")
-                            pass
-
-                if not isArchive and suspScore >= 70:
-                    notif_str = "Xylent is taking action against detected malware "+ path
+                            print(f"Error during YARA matching: {e}")
+            if not isArchive and suspScore >= 70:
+                    notif_str = "Xylent is taking action against detected malware " + path
                     from notifypy import Notify
                     notification = Notify()
                     notification.title = "Malware Detected"
                     notification.message = notif_str
                     notification.send()
-                    self.quar.quarantine(path,detectionSpace)
-                
-                if isArchive:
+                    self.quar.quarantine(path, detectionSpace)
+
+            if isArchive:
                     self.handleArchives(path)
 
-                return detectionSpace
-            else:
-                return "SKIPPED"
+            return detectionSpace
         except Exception as e:
             print(e)
             return "SKIPPED"
-        # TODO: if file is packed: additional detection
-        # TODO: if file is a .zip,.rar,.7z and other varients ADDITIONAL detections
-
-
-    def scanFolders(self,location):
+    def scanFolders(self, location):
         directories = []
         if isinstance(location, list):
             for target in location:
@@ -186,13 +187,10 @@ class Scanner:
             for (dirpath, dirnames, filenames) in os.walk(location):
                 directories += [os.path.join(dirpath, file) for file in filenames]
 
-        # Scan the files 
-        # TODO: investigate for better performance with binary search and other methods?
-        # TODO: develop a caching heuristic to scan files only after a certain age has passed
         scanReport = {}
         for files in directories:
             verdict = self.scanFile(files)
             if verdict:
-                print("Veridct is: "+verdict)
+                print("Verdict is: " + verdict)
                 scanReport[files] = verdict
-        return scanReport
+        return 
