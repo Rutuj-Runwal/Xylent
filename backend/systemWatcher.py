a7c6fffa1df1703a12b50eabbdf321827e5ebbc0
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-12-28 14:34:51 +0300

    Update systemWatcher.py

--- backend/systemWatcher.py
+++ backend/systemWatcher.py
@@ -3,7 +3,6 @@ import win32file
 import win32con
 import win32gui
 import win32api
-import win32evtlog
 import win32process
 import pynput.mouse
 import threading
@@ -11,7 +10,6 @@ import queue
 from queue import Queue
 import psutil
 import concurrent.futures
-import time
 from parseJson import ParseJson
 
 FILE_ACTION_ADDED = 0x00000001
@@ -20,85 +18,24 @@ FILE_ACTION_MODIFIED = 0x00000003
 
 # Initialize ParseJson
 XYLENT_NEW_PROCESS_INFO = ParseJson('./config', 'new_processes.json', {})
-printed_processes = set()  # Global variable to store printed processes
+
+# Add global declarations for 'printed_processes' and 'previous_list'
+printed_processes = set()
 previous_list = set()
-file_queue = Queue()
-mouse_queue = Queue()
-new_event_queue = Queue()
-
-def load_new_processes():
-    try:
-        return XYLENT_NEW_PROCESS_INFO.parseDataFile([])
-    except Exception:
-        return []
-
-def save_new_processes(new_processes):
-    XYLENT_NEW_PROCESS_INFO.setVal("new_processes", new_processes)
-
-def get_running_processes():
-    processes = set()
-    for p in psutil.process_iter(['exe', 'cmdline', 'ppid']):
-        try:
-            if p.info is not None and 'exe' in p.info:
-                exe = p.info['exe']
-                cmdline = tuple(p.info.get('cmdline', []))
-                ppid = p.info.get('ppid', None)
-                processes.add((exe, cmdline, ppid))
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess, TypeError):
-            pass  # Skip processes that are inaccessible or no longer exist
-        except Exception as e:
-            print(f"Error getting process info: {e}")
-    return processes
-
-def get_parent_process_info(file_path):
-    try:
-        process = psutil.Process(os.getpid())
-        for parent in process.parents():
-            if parent.exe() == file_path:
-                return {
-                    'name': parent.name(),
-                    'exe': parent.exe(),
-                    'cmdline': parent.cmdline(),
-                    'pid': parent.pid,
-                }
-        return None
-    except psutil.NoSuchProcess:
-        print(f"Error: No such process with path {file_path}")
-    except psutil.AccessDenied:
-        print(f"Error: Access denied while retrieving information for path {file_path}")
-    except Exception as e:
-        print(f"An unexpected error occurred while getting parent process info for path {file_path}: {e}")
-
-    return None
+results_queue = Queue()  # Define results_queue as a global variable
 
 def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
     XYLENT_SCAN_CACHE = ParseJson('./config', 'xylent_scancache', {})
     XYLENT_CACHE_MAXSIZE = 500000  # 500KB
-    def process_mouse_events():
-        while thread_resume.is_set():
-            try:
-                # Retrieve mouse events from the queue
-                path_to_scan = mouse_queue.get()
-                verdict = XylentScanner.scanFile(path_to_scan)
-                XYLENT_SCAN_CACHE.setVal(path_to_scan, verdict)
-            except Exception as e:
-                print(f"Error in process_mouse_events: {e}")
-    def process_new_process_events():
-         while thread_resume.is_set():
-            try:
-                # Retrieve mouse events from the queue
-                path_to_scan = new_event_queue.get()
-                verdict = XylentScanner.scanFile(path_to_scan)
-                XYLENT_SCAN_CACHE.setVal(path_to_scan, verdict)
-            except Exception as e:
-                print(f"Error in process_new_process_events: {e}")
-    def on_mouse_click(x, y, button, pressed):
-        if pressed:
-            path_to_scan = get_file_path_from_click(x, y)
-            print(f"Mouse clicked at ({x}, {y}) on file: {path_to_scan}")
+    file_queue = Queue()
 
-            # Add file to the queue for processing in the main thread
-            mouse_queue.put(path_to_scan)
+    def on_mouse_click(x, y, button, pressed):
+        path_to_scan = get_file_path_from_click(x, y)
+        print(f"Mouse clicked at ({x}, {y}) with button {button} on file: {path_to_scan}")
+    
+        if path_to_scan is not None:
+            result = XylentScanner.scanFile(path_to_scan)
+            results_queue.put(result)  # Put the result in the queue
 
     def get_file_path_from_click(x, y):
         hwnd = win32gui.WindowFromPoint((x, y))
@@ -109,16 +46,26 @@ def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
     def process_file_queue():
         while thread_resume.is_set():
             try:
-                path_to_scan = file_queue.get()
+                path_to_scan = file_queue.get()  # Timeout may cause lag ironically
                 print(f"Processing file: {path_to_scan}")
-                verdict = XylentScanner.scanFile(path_to_scan)
-                XYLENT_SCAN_CACHE.setVal(path_to_scan, verdict)
+
+                try:
+                    if os.path.isfile(path_to_scan):
+                        verdict = XylentScanner.scanFile(path_to_scan)
+                        XYLENT_SCAN_CACHE.setVal(path_to_scan, verdict)
+                        results_queue.put(verdict)  # Put the result in the queue
+                        print(f"Scanned and cached: {path_to_scan}")
+                except Exception as e:
+                    print(e)
+                    print(f"Error scanning {path_to_scan}")
+
             except queue.Empty:
                 pass  # Queue is empty, continue checking
 
             if os.path.getsize(XYLENT_SCAN_CACHE.PATH) >= XYLENT_CACHE_MAXSIZE:
                 XYLENT_SCAN_CACHE.purge()
                 print("Purging")
+
     def file_monitor():
         while thread_resume.is_set():
             # File monitoring
@@ -153,63 +100,9 @@ def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
             for action, file in results:
                 path_to_scan = os.path.join(path_to_watch, file)
                 print(path_to_scan)  # Print the path for debugging purposes
+                result = XylentScanner.scanFile(path_to_scan)
+                results_queue.put(result)  # Put the result in the queue
 
-                # Add file to the queue for processing in the main thread
-                file_queue.put(path_to_scan)
-
-            if os.path.getsize(XYLENT_SCAN_CACHE.PATH) >= XYLENT_CACHE_MAXSIZE:
-                XYLENT_SCAN_CACHE.purge()
-                print("Purging")
-    def new_process_checker(process_info):
-        global printed_processes
-
-        # process_info is a tuple (exe, cmdline, pid)
-        exe, cmdline, pid = process_info
-
-        if exe not in printed_processes:
-            # Print the running file only once
-            print(f"Running File: {exe}")
-            printed_processes.add(exe)
-
-            parent_process_info = get_parent_process_info(pid)
-            # If there is no parent process information or it has no executable information, skip processing
-            if parent_process_info is None or parent_process_info.get('exe') is None:
-                return  # Skip processing if parent process info is None or has no executable information
-
-            parent_path = parent_process_info['exe']
-
-            # Check if parent and child have the same location
-            if parent_path != "Unknown" and exe.startswith(parent_path):
-                return  # Skip processing if they have the same location
-
-            # Check if parent and child have the same full path
-            if os.path.abspath(exe) == os.path.abspath(parent_path):
-                return  # Skip processing if they have the same full path
-
-            message = f"Path: {exe}, Parent Process Path: {parent_path}, Command Line: {cmdline}"
-
-            # Print to the console
-            print("New Process Detected:", message)
-
-            # Check if the command line includes paths
-            if isinstance(cmdline, list):  # Ensure cmdline is a list
-                paths = [arg for arg in cmdline if os.path.isabs(arg) and os.path.exists(arg)]
-                if paths:
-                    print(f"Command Line includes paths: {paths}, scanning related folder for process {exe}")
-                    # Assuming you have a method named 'scanFile' in your Scanner class
-                    for path in paths:
-                        print(f"Scanning file: {path}")
-                        new_event_queue.put(path)
-            # Include the running file itself in the path_to_scan
-            path_to_scan = exe
-            print(f"Scanning file: {path_to_scan}")
-            new_event_queue.put(path_to_scan)
-
-            # Set verdict for the parent_path
-            new_event_queue.put(parent_path)
-
-        # Set verdict for the parent_path
-        new_event_queue.put(parent_path)
     def watch_processes():
         global printed_processes
         global previous_list
@@ -239,7 +132,7 @@ def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
                 if newly_started_processes:
                     with concurrent.futures.ThreadPoolExecutor() as executor:
                         # Submit each task individually and pass the required arguments
-                        futures = [executor.submit(new_process_checker, info) for info in newly_started_processes]
+                        futures = [executor.submit(new_process_checker, info, XylentScanner, results_queue) for info in newly_started_processes]
                         concurrent.futures.wait(futures)
 
                     # Print new processes once
@@ -256,22 +149,111 @@ def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
                 save_new_processes(list(new_processes))
             except Exception as e:
                 print(f"Error in watch_processes: {e}")
-     # Start the thread for processing mouse events concurrently
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        mouse_listener_future = executor.submit(lambda: pynput.mouse.Listener(on_click=on_mouse_click).start())
-        process_mouse_events_future = executor.submit(lambda: process_mouse_events())
 
-    # Start the thread for processing file events concurrently
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        file_monitor_future = executor.submit(lambda: file_monitor())
-        process_file_events_future = executor.submit(lambda: process_file_queue())
+    def load_new_processes():
+        try:
+            return XYLENT_NEW_PROCESS_INFO.parseDataFile([])
+        except Exception:
+            return []
 
-    # Start the watch processes thread concurrently
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        watch_processes_future = executor.submit(lambda: watch_processes())
-        new_process_future = executor.submit(lambda: process_new_process_events())
+    def save_new_processes(new_processes):
+        XYLENT_NEW_PROCESS_INFO.setVal("new_processes", new_processes)
 
-    # Wait for all threads to finish
-    concurrent.futures.wait([mouse_listener_future, process_mouse_events_future, file_monitor_future, process_file_events_future, watch_processes_future, new_process_future])
+    def get_running_processes():
+        processes = set()
+        for p in psutil.process_iter(['exe', 'cmdline', 'ppid']):
+            try:
+                if p.info is not None and 'exe' in p.info:
+                    exe = p.info['exe']
+                    cmdline = tuple(p.info.get('cmdline', []))
+                    ppid = p.info.get('ppid', None)
+                    processes.add((exe, cmdline, ppid))
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess, TypeError):
+                pass  # Skip processes that are inaccessible or no longer exist
+            except Exception as e:
+                print(f"Error getting process info: {e}")
+        return processes
+
+    def new_process_checker(process_info, XylentScanner, results_queue):
+        global printed_processes
+
+        # process_info is a tuple (exe, cmdline, pid)
+        exe, cmdline, pid = process_info
+
+        if exe not in printed_processes:
+            # Print the running file only once
+            print(f"Running File: {exe}")
+            printed_processes.add(exe)
+
+            parent_process_info = get_parent_process_info(pid)
+            if parent_process_info is None or parent_process_info.get('exe') is None:
+                return  # Skip processing if parent process info is None or has no executable information
+
+            parent_path = parent_process_info['exe']
+
+            # Check if parent and child have the same location
+            if parent_path != "Unknown" and exe.startswith(parent_path):
+                return  # Skip processing if they have the same location
+
+            # Check if parent and child have the same full path
+            if os.path.abspath(exe) == os.path.abspath(parent_path):
+                return  # Skip processing if they have the same full path
+
+            message = f"Path: {exe}, Parent Process Path: {parent_path}, Command Line: {cmdline}"
+
+            # Print to the console
+            print("New Process Detected:", message)
+
+            # Check if the command line includes paths
+            if isinstance(cmdline, list):  # Ensure cmdline is a list
+                paths = [arg for arg in cmdline if os.path.isabs(arg) and os.path.exists(arg)]
+                if paths:
+                    print(f"Command Line includes paths: {paths}, scanning related folder for process {exe}")
+                    # Assuming you have a method named 'scanFile' in your Scanner class
+                    for path in paths:
+                        result = XylentScanner.scanFile(path)
+                        results_queue.put(result)  # Put the result in the queue
+        # Include the running file itself in the path_to_scan
+        path_to_scan = exe
+        result = XylentScanner.scanFile(path_to_scan)
+        results_queue.put(result)  # Put the result in the queue
+
+    def get_parent_process_info(file_path):
+        try:
+            process = psutil.Process(os.getpid())
+            for parent in process.parents():
+                if parent.exe() == file_path:
+                    return {
+                        'name': parent.name(),
+                        'exe': parent.exe(),
+                        'cmdline': parent.cmdline(),
+                        'pid': parent.pid,
+                    }
+            return None
+        except psutil.NoSuchProcess:
+            print(f"Error: No such process with path {file_path}")
+        except psutil.AccessDenied:
+            print(f"Error: Access denied while retrieving information for path {file_path}")
+        except Exception as e:
+            print(f"An unexpected error occurred while getting parent process info for path {file_path}: {e}")
+
+        return None
+
+    mouse_listener = threading.Thread(target=lambda: pynput.mouse.Listener(on_click=on_mouse_click).start())
+    mouse_listener.start()
+
+    monitor_thread = threading.Thread(target=file_monitor)
+    monitor_thread.start()
+
+    process_queue_thread = threading.Thread(target=process_file_queue)
+    process_queue_thread.start()
+
+    watch_processes_thread = threading.Thread(target=watch_processes)
+    watch_processes_thread.start()
+
+    mouse_listener.join()  # Wait for the mouse listener to finish (shouldn't happen in this case)
+    monitor_thread.join()  # Wait for the file monitor to finish
+    process_queue_thread.join()  # Wait for the file processing thread to finish
+    watch_processes_thread.join()  # Wait for the process monitoring thread to finish
 
     print("RTP waiting to start...")
