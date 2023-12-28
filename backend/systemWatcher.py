a2490b477e6354922502cab5988ce1e39dc96e59
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-12-28 21:56:42 +0300

    Update systemWatcher.py

--- backend/systemWatcher.py
+++ backend/systemWatcher.py
@@ -98,8 +98,8 @@ def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
 
     def on_mouse_click(x, y, button, pressed):
      while thread_resume.wait():
-        path_to_scan = get_file_path_from_click(x, y)
-        print(f"Mouse clicked at ({x}, {y}) with button {button} on file: {path_to_scan}")
+      path_to_scan = get_file_path_from_click(x, y)
+     print(f"Mouse clicked at ({x}, {y}) with button {button} on file: {path_to_scan}")
     
      if path_to_scan is not None:
             mouse_queue.put(path_to_scan)  # Put the result in the queue
