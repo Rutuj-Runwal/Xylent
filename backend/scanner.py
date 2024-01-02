34fe222836d278869718986bb869bdcba94efe17
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-01-02 17:32:32 +0300

    Update scanner.py

--- backend/scanner.py
+++ backend/scanner.py
@@ -4,11 +4,12 @@ import tlsh
 from quarantineThreats import Quarantine
 
 class Scanner:
-    def __init__(self, sha256_signatures, md5_signatures, sha1_signatures, tlsh_signatures, rootPath, yara_rules):
+    def __init__(self, sha256_signatures, md5_signatures, sha1_signatures, tlsh_signatures, rootPath, yara_rules, virusshare_md5_signatures):
         self.__sha256_signatures = sha256_signatures
         self.__md5_signatures = md5_signatures
         self.__sha1_signatures = sha1_signatures
         self.__tlsh_signatures = tlsh_signatures
+        self.virusshare_md5_signatures_data = virusshare_md5_signatures
         self.__rootPath = rootPath
         self.yara_rules = yara_rules
         self.quarantineData = {
@@ -23,19 +24,6 @@ class Scanner:
         excluded_rules_path = os.path.join(self.__rootPath, 'excluded', 'excluded_rules.txt')
         with open(excluded_rules_path, "r") as file:
             self.excluded_rules = file.read()
-    def read_sha1_signatures(self, file_path):
-        sha1_signatures = {}
-        try:
-            with open(file_path, 'r') as file:
-                for line in file:
-                    parts = line.strip().split(' ')
-                    if len(parts) == 2:
-                        sha1_signatures[parts[0]] = parts[1]
-        except FileNotFoundError:
-            print(f"File not found: {file_path}")
-        except Exception as e:
-            print(f"Error reading SHA-1 signatures: {e}")
-        return sha1_signatures
 
     def getSHA1Hash(self, path):
         try:
@@ -51,16 +39,17 @@ class Scanner:
         except (PermissionError, OSError):
             print("Permission Error")
             return "XYLENT_PERMISSION_ERROR"
+
     def getFileHash(self, path):
         try:
             with open(path, 'rb') as f:
                 bytes = f.read()
-                
+
                 # Check if the file is empty
                 if not bytes:
                     print("File is empty. Skipping hash calculation.")
                     return None  # Return None for an empty file
-                
+
                 hash = hashlib.sha256(bytes).hexdigest()
                 return hash
         except (PermissionError, OSError):
@@ -71,32 +60,32 @@ class Scanner:
         try:
             with open(path, 'rb') as f:
                 bytes = f.read()
-                
+
                 # Check if the file is empty
                 if not bytes:
                     print("File is empty. Skipping hash calculation.")
                     return None  # Return None for an empty file
-                
+
                 hash = hashlib.md5(bytes).hexdigest()
                 return hash
         except (PermissionError, OSError):
             print("Permission Error")
             return "XYLENT_PERMISSION_ERROR"
-         
+
     def calculate_tlsh(self, file_path):
-     try:
-        with open(file_path, "rb") as file:
-            file_data = file.read()
-        if file_data:
-            tlsh_value = tlsh.hash(file_data)
-            return tlsh_value
-        else:
-            print("File is empty. Skipping TLSH hash calculation.")
+        try:
+            with open(file_path, "rb") as file:
+                file_data = file.read()
+            if file_data:
+                tlsh_value = tlsh.hash(file_data)
+                return tlsh_value
+            else:
+                print("File is empty. Skipping TLSH hash calculation.")
+                return None
+        except (PermissionError, OSError):
+            print("Permission Error or OS Error. Skipping TLSH hash calculation.")
             return None
-     except (PermissionError, OSError):
-        print("Permission Error or OS Error. Skipping TLSH hash calculation.")
-        return None
-     
+
     def getTLSHHash(self, path):
         try:
             with open(path, 'rb') as f:
@@ -106,11 +95,11 @@ class Scanner:
                 if not file_size <= 256:
                     print("File size is 256 bytes or less. Skipping TLSH hash calculation.")
                     return None  # Return None for an empty file
-                
+
         except (PermissionError, OSError):
             print("Permission Error or OS Error. Skipping TLSH hash calculation.")
             return None
-        
+
     def verifyExecutableSignature(self, path):
         import subprocess
         import time
@@ -166,20 +155,20 @@ class Scanner:
             fileExtension = os.path.splitext(path)[1]
             file_size = os.path.getsize(path)
             hashToChk = self.getFileHash(path)
-          # Check if the file is empty
+            # Check if the file is empty
             if hashToChk is None:
-              print("File is empty. Skipping.")
-              return "SKIPPED"
+                print("File is empty. Skipping.")
+                return "SKIPPED"
             # Check if the file is empty or size is 4 bytes or less
             if file_size <= 4:
-             print("File size is 4 bytes or less. Skipping.")
-             return "SKIPPED"
+                print("File size is 4 bytes or less. Skipping.")
+                return "SKIPPED"
 
             if hashToChk == "XYLENT_PERMISSION_ERROR":
-                    return "SKIPPED"
+                return "SKIPPED"
 
             if fileExtension == ".zip" or fileExtension == ".tar":
-                    isArchive = True
+                isArchive = True
 
             if not isArchive and (fileExtension == ".exe" or fileExtension == ".msi"):
                 print(path)
@@ -216,7 +205,7 @@ class Scanner:
                     # Set suspScore to 100 or any other value as needed
                     suspScore = 100
             if hashToChk != "" and suspScore < 70:
-               # TLSH BASED DETECTION
+                # TLSH BASED DETECTION
                 tlsh_match_found = False
                 tlsh_hash = self.getTLSHHash(path)
                 if tlsh_hash is not None and tlsh_hash != "TNULL":
@@ -227,9 +216,9 @@ class Scanner:
                                 detectionSpace = "[S]" + self.__tlsh_signatures[tlsh_sig]
                                 tlsh_match_found = True
                                 print(f"Malware detected using TLSH! Signature: {tlsh_sig}, Similarity: {similarity}")
- 
+
                 if tlsh_match_found:
-                  suspScore = 100
+                    suspScore = 100
             if hashToChk != "" and suspScore < 70:
                 # SIGNATURE BASED DETECTION - SHA1
                 sha1_match_found = False
@@ -239,11 +228,21 @@ class Scanner:
                         print(self.__sha1_signatures[sha1_hash_sig])
                         detectionSpace = "[S]" + self.__sha1_signatures[sha1_hash_sig]
                         sha1_match_found = True
+            if hashToChk != "" and suspScore < 70:
+                # VIRUSSHARE.TXT BASED DETECTION - MD5
+                virusshare_match_found = False
+                md5_hash = self.getMD5Hash(path)
+                if md5_hash in self.virusshare_md5_signatures_data:
+                    detectionSpace = "[S] + VirusShare"  # You can add more specific information if available
+                    virusshare_match_found = True
+                # Combine hash match checks
+                if virusshare_match_found:
+                    # Set suspScore to 100 or any other value as needed
+                    suspScore = 100
                 # Combine hash match checks
                 if sha1_match_found:
                     # Set suspScore to 100 or any other value as needed
                     suspScore = 100
-
                 # YARA RULES DETECTION
                 if not isArchive and suspScore < 70:
                     try:
