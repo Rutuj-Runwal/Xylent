005da2db09b13f7844aa1807882be0fc3936fdee
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-01-04 22:25:13 +0300

    Update systemWatcher.py

--- backend/systemWatcher.py
+++ backend/systemWatcher.py
@@ -17,10 +17,6 @@ XYLENT_NEW_PROCESS_INFO = ParseJson('./config', 'new_processes.json', {})
 printed_processes = set()
 previous_list = set()
 results_queue = Queue()  # Define results_queue as a global variable
-# Add a queue for mouse clicks
-mouse_click_queue = Queue()
-# Add a queue for watch processes
-watch_queue = Queue()
 def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
     XYLENT_SCAN_CACHE = ParseJson('./config', 'xylent_scancache', {})
 
@@ -144,7 +140,7 @@ def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
             print(f"Running File: {exe}")
             printed_processes.add(exe)
             result0 = XylentScanner.scanFile(exe)
-            watch_queue.put(result0)  # Put the result in the queue
+            results_queue.put(result0)  # Put the result in the queue
             XYLENT_SCAN_CACHE.setVal(exe,result0)
             parent_process_info = get_parent_process_info(pid)
             if parent_process_info is None or parent_process_info.get('exe') is None:
@@ -162,7 +158,7 @@ def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
 
             message = f"Path: {exe}, Parent Process Path: {parent_path}, Command Line: {cmdline}"
             result = XylentScanner.scanFile(parent_path)
-            watch_queue.put(result)  # Put the result in the queue
+            results_queue.put(result)  # Put the result in the queue
             XYLENT_SCAN_CACHE.setVal(parent_path,result)
             # Print to the console
             print("New Process Detected:", message)
@@ -175,7 +171,7 @@ def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
                     # Assuming you have a method named 'scanFile' in your Scanner class
                     for path in paths:
                         result1 = XylentScanner.scanFile(path)
-                        watch_queue.put(result1)  # Put the result in the queue
+                        results_queue.put(result1)  # Put the result in the queue
                         XYLENT_SCAN_CACHE.setVal(path,result1)
 
     def get_parent_process_info(file_path):
