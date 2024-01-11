67489dd92a91e48ef9e246ae384f1beea76bc2bb
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-01-11 16:24:31 +0000

    Update systemWatcher.py

--- backend/systemWatcher.py
+++ backend/systemWatcher.py
@@ -1,6 +1,7 @@
 import os
 from queue import Queue
 import subprocess
+import threading
 
 verdict_queue = Queue()
 
@@ -10,24 +11,36 @@ def systemWatcher(XylentScanner, SYSTEM_DRIVE, thread_resume):
     monitor_exe_path = os.path.abspath('.\\monitor\\x64\\Debug\\monitor.exe')
     subprocess.Popen([monitor_exe_path, os.path.abspath(SYSTEM_DRIVE)])
 
+    output_txt_path = "output.txt"
+    last_position = 0
+
     while thread_resume.is_set():
         try:
-            output_txt_path = "output.txt"
-
             with open(output_txt_path, "r") as file:
+                file.seek(last_position)
                 changes = file.readlines()
 
-            for change in changes:
-                pathToScan = os.path.abspath(os.path.join(SYSTEM_DRIVE, change.strip()))
-                print(pathToScan)
+                if not changes:
+                    # No new data, block until new lines are added
+                    file.readline()
 
-                try:
-                    if pathToScan:
-                        verdict = XylentScanner.scanFile(pathToScan)
-                        verdict_queue.put(verdict)  # Put the result in the queue
-                except Exception as e:
-                    print(e)
-                    print(f"Error processing {pathToScan}")
+                    # Move to the end of the file for the next iteration
+                    last_position = file.tell()
+                else:
+                    for change in changes:
+                        pathToScan = os.path.abspath(os.path.join(SYSTEM_DRIVE, change.strip()))
+                        print(pathToScan)
+
+                        try:
+                            if pathToScan:
+                                verdict = XylentScanner.scanFile(pathToScan)
+                                verdict_queue.put(verdict)  # Put the result in the queue
+                        except Exception as e:
+                            print(e)
+                            print(f"Error processing {pathToScan}")
+
+                    # Update the last position to the end of the file
+                    last_position = file.tell()
         except Exception as e:
             print(e)
 
