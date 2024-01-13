1637dccc18ba126b46ea6d557e018799d163d669
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-01-13 14:56:39 +0300

    Update systemWatcher.py

--- backend/systemWatcher.py
+++ backend/systemWatcher.py
@@ -5,6 +5,7 @@ import threading
 import subprocess
 import psutil
 import shutil
+import time
 
 verdict_queue = Queue()
 
@@ -14,7 +15,7 @@ def get_all_running_files():
         running_files.append(proc.info['exe'])
     return running_files
 
-def systemWatcher(XylentScanner,thread_resume):
+def systemWatcher(XylentScanner, thread_resume):
     output_txt_path = "output.txt"
     output_copy_path = "output_copy.txt"
 
@@ -25,7 +26,11 @@ def systemWatcher(XylentScanner,thread_resume):
     monitor_exe_path = os.path.abspath('.\\monitor\\target\\debug\\monitor.exe')
     subprocess.Popen([monitor_exe_path])
 
+    scanned_files = set()  # Keep track of scanned files
+    last_cleanup_time = time.time()  # Initialize the last cleanup time
+
     def scan_changes():
+        nonlocal scanned_files, last_cleanup_time  # Use the scanned_files and last_cleanup_time from the outer function
         last_position = 0
         with ThreadPoolExecutor(max_workers=10000) as executor:
             while thread_resume.is_set():
@@ -42,16 +47,23 @@ def systemWatcher(XylentScanner,thread_resume):
                             path_to_scan = os.path.abspath(change.strip())
                             print(path_to_scan)
 
-                            if os.path.exists(path_to_scan):
+                            if os.path.exists(path_to_scan) and path_to_scan not in scanned_files:
                                 # Process the path using ThreadPoolExecutor
                                 executor.submit(XylentScanner.scanFile, path_to_scan)
+                                scanned_files.add(path_to_scan)
 
                     # Update the last position to the end of the file
                     last_position = file.tell()
 
+                    # Check if it's time to clean up the scanned_files set (every 60 seconds)
+                    current_time = time.time()
+                    if current_time - last_cleanup_time >= 60:
+                        scanned_files.clear()  # Clear the set
+                        last_cleanup_time = current_time
+
                 except Exception as e:
                     print(e)
-
+                
     # Start the scanning process in a separate thread
     threading.Thread(target=scan_changes).start()
 
